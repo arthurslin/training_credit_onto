197fe109efab4e0fc2b184329ffc9d59112ace18
Arthur Lin <arthurslin.work@gmail.com>
2024-07-30 12:25:27 -0700

    credit script bugfix & completion

--- training_credit_onto.py
+++ training_credit_onto.py
@@ -1,4 +1,5 @@
 import pandas as pd
+# import xlsxwriter
 import glob
 import os
 
@@ -6,7 +7,7 @@ directories = ["log_o", "log_sap"]
 oracle_items = ["Customer Name", "Customer PO", "Product Family",
                 "Part Number", "Region", "Order Quantity", "Line Total(USD)", "Promise Date"]
 sap_items = ["Sold-To Party Name",
-             "Order Quantity (Item)", "Net Value (Item)", "Delivery Date"]
+             "Order Quantity (Item)","Material", "Net Value (Item)", "Delivery Date"]
 
 
 def load_data():
@@ -17,8 +18,7 @@ def load_data():
             raise FileNotFoundError(i, "File not found")
         for path in paths:
             xl = pd.ExcelFile(path)
-            for sheet in xl.sheet_names:
-                df = pd.read_excel(path, sheet_name=sheet)
+            df = pd.read_excel(path, sheet_name=xl.sheet_names[0])
         to_return.append(df)
     return to_return
 
@@ -32,8 +32,9 @@ def get_credits(data):
     training_items = credits["Customer PO"].tolist()
     training_orders = log_o[log_o['Customer PO'].isin(training_items)]
 
+    print(log_sap)
     log_sap["Material"] = log_sap["Material"].astype(str)
-    credits_sap = log_sap
+    # credits_sap = log_sap
     # credits_sap = log_sap.drop(log_sap[log_sap['Material'] != '778752'].index, inplace=False)
 
     # cleaned_orders = training_orders.loc[~(training_orders['Part Number'] == '778752')].filter(oracle_items)
@@ -43,19 +44,25 @@ def get_credits(data):
     cleaned_orders["Promise Date"] = (pd.to_datetime(
         cleaned_orders["Promise Date"], dayfirst=True)).dt.date
     cleaned_orders = cleaned_orders[
-        (cleaned_orders['Line Total(USD)'] != 0) |
+        (cleaned_orders['Line Total(USD)'] >= 1) |
         (cleaned_orders['Part Number'] == '778752')
     ]
 
-    cleaned_orders_sap = credits_sap.filter(sap_items)
+    cleaned_orders_sap = log_sap[sap_items]
     cleaned_orders_sap["Quarter"] = pd.to_datetime(
         cleaned_orders_sap["Delivery Date"], dayfirst=True).dt.quarter
     cleaned_orders_sap["Delivery Date"] = pd.to_datetime(
         cleaned_orders_sap["Delivery Date"], dayfirst=True).dt.date
+    
+    cleaned_orders.loc[cleaned_orders['Part Number'] == '778752', 'Total Credit Price'] = cleaned_orders['Order Quantity'] * 7500
+    cleaned_orders_sap['Total Credit Price'] = cleaned_orders_sap['Order Quantity (Item)'] * 7500
+
+
 
     with pd.ExcelWriter("training_items.xlsx") as writer:
         cleaned_orders.to_excel(writer, sheet_name="oracle", index=False)
+        # writer.sheets["oracle"].autofilter(0,0,cleaned_orders.shape[0],cleaned_orders.shape[1])
         cleaned_orders_sap.to_excel(writer, sheet_name="sap", index=False)
-
+        # writer.sheets["sap"].autofilter(0,0,cleaned_orders_sap.shape[0],cleaned_orders_sap.shape[1])
 
 get_credits(load_data())
